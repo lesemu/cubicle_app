265b03018174afd2dc349ae31973c338a037bf27
leon <leon@boldmovestudio.com>
2012-01-07 15:20:28 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CubicleApp::Application.config.secret_token = 'a97b496ba4f4f6842fa6af39df952013dbc71a6bbe950dae5b65e5a1aed640d08d5b4f2db0f561b072b71927d3a6beb48ab41df8ef3c51afa35a37ca38529645'
